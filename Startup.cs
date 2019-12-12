e1749d05862f7045bdd6beb2f1fa13fb11761837
sikira <cgisikira@gmail.com>
2019-12-12 20:08:40 +0100

    after scaffold, logout not working

--- Startup.cs
+++ Startup.cs
@@ -31,6 +31,8 @@ namespace BlazorScaffoldedIdentity
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
+
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlite(
                     Configuration.GetConnectionString("DefaultConnection")));
@@ -40,6 +42,8 @@ namespace BlazorScaffoldedIdentity
             services.AddServerSideBlazor();
             services.AddScoped<AuthenticationStateProvider, RevalidatingIdentityAuthenticationStateProvider<IdentityUser>>();
             services.AddSingleton<WeatherForecastService>();
+
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.

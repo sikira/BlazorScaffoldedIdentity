5b8e3bfda85c25c92bf5fd5bf0c97cdffc2f858a
sikira <cgisikira@gmail.com>
2020-01-04 16:32:39 +0100

    add ignoreantiforgeryattribue

---
+++ Areas/Identity/Pages/Account/LogoutNotUsing.cshtml.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace BlazorScaffoldedIdentity.Areas.Identity.Pages.Account
+{
+    [IgnoreAntiforgeryToken]
+    [AllowAnonymous]
+    public class LogoutModel : PageModel
+    {
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly ILogger<LogoutModel> _logger;
+
+        public LogoutModel(SignInManager<IdentityUser> signInManager, ILogger<LogoutModel> logger)
+        {
+            _signInManager = signInManager;
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+
+        public async Task<IActionResult> OnPost(string returnUrl = null)
+        {
+            await _signInManager.SignOutAsync();
+            _logger.LogInformation("User logged out.");
+            if (returnUrl != null)
+            {
+                 _logger.LogInformation("With url");
+                return LocalRedirect(returnUrl);
+            }
+            else
+            {
+                 _logger.LogInformation("Without url");
+                 return Redirect("~/");
+                // return RedirectToPage();
+            }
+        }
+    }
+}
